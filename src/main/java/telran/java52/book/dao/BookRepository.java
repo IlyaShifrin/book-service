b9f4e9d8de26f0f4c23ca3b98f913988019eed40
Ilya <drunktrumpet1@gmail.com>
2024-06-15 15:53:08 +0300

    First commit

--- src/main/java/telran/java52/book/dao/BookRepository.java
+++ src/main/java/telran/java52/book/dao/BookRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import telran.java52.book.model.Book;
 
 
-
 public interface BookRepository extends JpaRepository<Book, String> {
 	
 	Stream<Book> findByAuthorsName(String name);
